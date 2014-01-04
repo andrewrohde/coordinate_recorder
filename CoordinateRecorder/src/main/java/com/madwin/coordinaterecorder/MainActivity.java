74459a9ca4988d21dd0fa89eecdfbd7f1e6973fb
arohde89 <a.rohde89@gmail.com>
2014-01-04 16:22:05 -0600

    reloaded project, added immersive move for kitkat

--- CoordinateRecorder/src/main/java/com/madwin/coordinaterecorder/MainActivity.java
+++ CoordinateRecorder/src/main/java/com/madwin/coordinaterecorder/MainActivity.java
@@ -193,4 +193,18 @@ public class MainActivity extends Activity {
         return (int) (location.getSpeed() * 2.23694);
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            View decorView = this.getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
+
 }
