c9030818578b3d0598062463c6ce85e66a00be98
arohde89 <a.rohde89@gmail.com>
2014-01-11 20:07:48 -0800

    re enable layout navigation

--- CoordinateRecorder/src/main/java/com/madwin/coordinaterecorder/MainActivity.java
+++ CoordinateRecorder/src/main/java/com/madwin/coordinaterecorder/MainActivity.java
@@ -203,7 +203,7 @@ public class MainActivity extends Activity {
             View decorView = this.getWindow().getDecorView();
             decorView.setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                          //  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
